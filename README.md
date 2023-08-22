94afaff81e7cc284a304a35c3160d5db1033926e
Sudocker <sudhakar82@gmail.com>
2023-08-22 10:46:14 +0530

    Added readme

---
+++ README.md
@@ -0,0 +1,13 @@
+This is a small kitchen sink app I build to learn langchain
+
+This assumes you are running local Llama (see https://github.com/jmorganca/ollama) and have a working ChatGPT API key
+
+To run this app
+
+```
+flask --app app.py run --debug
+```
+
+Load the url `http://localhost:5000/demo1` to see the first demo
+
+![Screenshot](./docs/demo1.png)
--- demo0/llama_cls.py
+++ demo0/llama_cls.py
@@ -2,7 +2,7 @@ from langchain.llms import Ollama
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-llm = Ollama(model="llama2", 
+llm = Ollama(model="llama2",
             base_url="http://localhost:11434", 
             callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
 
