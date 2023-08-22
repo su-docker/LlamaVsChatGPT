28fa58ab793b30a69b787842052e3ebc8e074b6d
Sudocker <sudhakar82@gmail.com>
2023-08-22 11:22:10 +0530

    Returing a nice error message with openai api key is invalid
    Some small UI changes

--- chatgpt.py
+++ chatgpt.py
@@ -1,10 +1,14 @@
 from langchain.chat_models import ChatOpenAI
+from openai.error import AuthenticationError
 from mysecrets import OPENAI_KEY
 
 class ChatGPTModel:
     
     def respond(self, question):
-      print("Question to ChatGPT: ", question)
-      chat_model = ChatOpenAI(openai_api_key=OPENAI_KEY)
-      response = chat_model.predict(question)
-      return response
+      try:
+        print("Question to ChatGPT: ", question)
+        chat_model = ChatOpenAI(openai_api_key=OPENAI_KEY)
+        response = chat_model.predict(question)
+        return response
+      except AuthenticationError:
+         return "Looks like your openai key is not valid. Please check"
--- mysecrets.py
+++ mysecrets.py
@@ -1,1 +1,2 @@
+# This key is just a placeholder. Update it with your key
 OPENAI_KEY = "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"
